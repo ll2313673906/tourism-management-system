e7d469d9158768bf930ca95ed3dc66091073d552
ll2313673906 <3340624204@qq.com>
2021-03-06 22:43:34 +0800

    项目搭建

---
+++ src/router/index.js
@@ -0,0 +1,184 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+import Layout from "@/layout";
+
+export const constantRoutes = [
+  {
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true
+  },
+
+  {
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true
+  },
+
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "首页",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "首页", icon: "dashboard" }
+      }
+    ]
+  },
+
+  {
+    path: "/example",
+    component: Layout,
+    redirect: "/example/table",
+    name: "用户管理",
+    meta: { title: "用户管理", icon: "el-icon-s-help" },
+    children: [
+      {
+        path: "table",
+        name: "角色信息",
+        component: () => import("@/views/table/index"),
+        meta: { title: "角色信息", icon: "table" }
+      },
+      {
+        path: "tree",
+        name: "用户信息",
+        component: () => import("@/views/tree/index"),
+        meta: { title: "用户信息", icon: "tree" }
+      },
+
+      {
+        path: "tree",
+        name: "会员信息",
+        component: () => import("@/views/tree/index"),
+        meta: { title: "会员信息", icon: "tree" }
+      }
+    ]
+  },
+
+  {
+    path: "/form",
+    component: Layout,
+    redirect: "/form/table",
+    name: "景点管理",
+    meta: { title: "景点管理", icon: "el-icon-s-help" },
+    children: [
+      {
+        path: "table",
+        name: "景点信息",
+        component: () => import("@/views/table/index"),
+        meta: { title: "景点信息", icon: "table" }
+      },
+      {
+        path: "tree",
+        name: "热门景点",
+        component: () => import("@/views/tree/index"),
+        meta: { title: "热门景点", icon: "tree" }
+      },
+
+      {
+        path: "tree",
+        name: "票价管理",
+        component: () => import("@/views/tree/index"),
+        meta: { title: "票价管理", icon: "tree" }
+      }
+    ]
+  },
+
+  {
+    path: "/nested",
+    component: Layout,
+    redirect: "/nested/menu1",
+    name: "Nested",
+    meta: {
+      title: "Nested",
+      icon: "nested"
+    },
+    children: [
+      {
+        path: "menu1",
+        component: () => import("@/views/nested/menu1/index"), // Parent router-view
+        name: "Menu1",
+        meta: { title: "Menu1" },
+        children: [
+          {
+            path: "menu1-1",
+            component: () => import("@/views/nested/menu1/menu1-1"),
+            name: "Menu1-1",
+            meta: { title: "Menu1-1" }
+          },
+          {
+            path: "menu1-2",
+            component: () => import("@/views/nested/menu1/menu1-2"),
+            name: "Menu1-2",
+            meta: { title: "Menu1-2" },
+            children: [
+              {
+                path: "menu1-2-1",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-1"),
+                name: "Menu1-2-1",
+                meta: { title: "Menu1-2-1" }
+              },
+              {
+                path: "menu1-2-2",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-2"),
+                name: "Menu1-2-2",
+                meta: { title: "Menu1-2-2" }
+              }
+            ]
+          },
+          {
+            path: "menu1-3",
+            component: () => import("@/views/nested/menu1/menu1-3"),
+            name: "Menu1-3",
+            meta: { title: "Menu1-3" }
+          }
+        ]
+      },
+      {
+        path: "menu2",
+        component: () => import("@/views/nested/menu2/index"),
+        name: "Menu2",
+        meta: { title: "menu2" }
+      }
+    ]
+  },
+
+  {
+    path: "external-link",
+    component: Layout,
+    children: [
+      {
+        path: "https://panjiachen.github.io/vue-element-admin-site/#/",
+        meta: { title: "External Link", icon: "link" }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: "*", redirect: "/404", hidden: true }
+];
+
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  });
+
+const router = createRouter();
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
+}
+
+export default router;
