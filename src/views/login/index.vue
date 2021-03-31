9af526ff6a3b901251708a0c53d5e6c252dadd5d
ll2313673906 <3340624204@qq.com>
2021-03-31 22:08:42 +0800

    项目搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,15 +4,7 @@
         <div class="login-main flex-row-between">
             <div class="left"></div>
             <!-- 注册表单 -->
-            <el-form
-                v-if="isShow"
-                ref="loginForm"
-                :model="loginForm"
-                :rules="loginRules"
-                class="login-form"
-                auto-complete="on"
-                label-position="left"
-            >
+            <el-form v-if="isShow" ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
                 <div class="title-container">
                     <h3 class="title">注册登录</h3>
                 </div>
@@ -20,58 +12,25 @@
                     <span class="svg-container">
                         <svg-icon icon-class="user" />
                     </span>
-                    <el-input
-                        ref="username"
-                        v-model="loginForm.username"
-                        placeholder="Username"
-                        name="username"
-                        type="text"
-                        tabindex="1"
-                        auto-complete="on"
-                    />
+                    <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text" tabindex="1" auto-complete="on" />
                 </el-form-item>
 
                 <el-form-item prop="password">
                     <span class="svg-container">
                         <svg-icon icon-class="password" />
                     </span>
-                    <el-input
-                        :key="passwordType"
-                        ref="password"
-                        v-model="loginForm.password"
-                        :type="passwordType"
-                        placeholder="Password"
-                        name="password"
-                        tabindex="2"
-                        auto-complete="on"
-                        @keyup.enter.native="handleLogin"
-                    />
-                    <span
-                        class="show-pwd"
-                        @click="showPwd"
-                    >
+                    <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
+                    <span class="show-pwd" @click="showPwd">
                         <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
                     </span>
                 </el-form-item>
                 <!-- 登录注册切换按钮 -->
                 <el-form-item>
-                    <el-button
-                        size="mini"
-                        type="text"
-                        @click="isShow= !isShow"
-                    >登录</el-button>
+                    <el-button size="mini" type="text" @click="isShow= !isShow">登录</el-button>
                 </el-form-item>
             </el-form>
             <!-- 登录表单 -->
-            <el-form
-                v-else
-                ref="loginForm"
-                :model="loginForm"
-                :rules="loginRules"
-                class="login-form"
-                auto-complete="on"
-                label-position="left"
-            >
+            <el-form v-else ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
                 <div class="title-container">
                     <h3 class="title">欢迎登录</h3>
@@ -81,54 +40,24 @@
                     <span class="svg-container">
                         <svg-icon icon-class="user" />
                     </span>
-                    <el-input
-                        ref="username"
-                        v-model="loginForm.username"
-                        placeholder="Username"
-                        name="username"
-                        type="text"
-                        tabindex="1"
-                        auto-complete="on"
-                    />
+                    <el-input ref="username" v-model="loginForm.username" placeholder="Username" name="username" type="text" tabindex="1" auto-complete="on" />
                 </el-form-item>
 
                 <el-form-item prop="password">
                     <span class="svg-container">
                         <svg-icon icon-class="password" />
                     </span>
-                    <el-input
-                        :key="passwordType"
-                        ref="password"
-                        v-model="loginForm.password"
-                        :type="passwordType"
-                        placeholder="Password"
-                        name="password"
-                        tabindex="2"
-                        auto-complete="on"
-                        @keyup.enter.native="handleLogin"
-                    />
-                    <span
-                        class="show-pwd"
-                        @click="showPwd"
-                    >
+                    <el-input :key="passwordType" ref="password" v-model="loginForm.password" :type="passwordType" placeholder="Password" name="password" tabindex="2" auto-complete="on" @keyup.enter.native="handleLogin" />
+                    <span class="show-pwd" @click="showPwd">
                         <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
                     </span>
                 </el-form-item>
 
-                <el-button
-                    :loading="loading"
-                    type="primary"
-                    style="width:100%;margin-bottom:30px;"
-                    @click.native.prevent="handleLogin"
-                >登录</el-button>
+                <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
                 <!-- 登录注册切换按钮 -->
                 <el-form-item>
-                    <el-button
-                        size="mini"
-                        type="text"
-                        @click="isShow= !isShow"
-                    >注册</el-button>
+                    <el-button size="mini" type="text" @click="isShow= !isShow">注册</el-button>
                 </el-form-item>
 
             </el-form>
@@ -194,21 +123,23 @@
             handleLogin() {
                 this.$refs.loginForm.validate((valid) => {
                     if (valid) {
-                        this.loading = true;
-                        this.$store
-                            .dispatch('user/login', this.loginForm)
-                            .then(() => {
-                                this.$router.push({ path: this.redirect || '/' });
-                                this.loading = false;
-                            })
-                            .catch(() => {
-                                this.loading = false;
-                            });
+                        // this.loading = true;
+                        // this.$store
+                        //     .dispatch('/login', this.loginForm)
+                        //     .then(() => {
+                        //         this.$router.push({ path: this.redirect || '/dashboard' });
+                        //         this.loading = false;
+                        //     })
+                        //     .catch(() => {
+                        //         this.loading = false;
+                        //     });
+                        // this.$router.push("/dashboard")
+
                     } else {
-                        console.log('error submit!!');
                         return false;
                     }
                 });
+                this.$router.push("/dashboard/dashboard")
             },
         },
     };
@@ -272,8 +203,8 @@ $light_gray: #eee;
     background-color: $bg;
     overflow: hidden;
     // background-image: url('https://i0.hippopx.com/photos/963/130/574/mountains-passes-clouds-mist-thumb.jpg');
-    background-image: url('https://i0.hippopx.com/photos/563/564/864/anime-late-stage-the-scenery-preview.jpg');
-    background-image: url('https://i0.hippopx.com/photos/761/692/1010/grass-green-nature-lawn-preview.jpg');
+    background-image: url("https://i0.hippopx.com/photos/563/564/864/anime-late-stage-the-scenery-preview.jpg");
+    background-image: url("https://i0.hippopx.com/photos/761/692/1010/grass-green-nature-lawn-preview.jpg");
     background-repeat: no-repeat;
     background-size: 100% 100%;
     display: flex;
@@ -283,11 +214,12 @@ $light_gray: #eee;
     // filter: blur(10px);
     .login-main {
         width: 50%;
-        background-image: url('https://i0.hippopx.com/photos/247/324/14/tour-sightseeing-landscape-travel-preview.jpg');
+        background-image: url("https://i0.hippopx.com/photos/247/324/14/tour-sightseeing-landscape-travel-preview.jpg");
         background-repeat: no-repeat;
         background-size: cover;
         border: 1px solid $dark_gray;
-        box-shadow: 0 1px 4px rgba(27, 216, 84, 0.3), 0 0 20px rgba(0, 0, 0, 0.1) inset;
+        box-shadow: 0 1px 4px rgba(27, 216, 84, 0.3),
+            0 0 20px rgba(0, 0, 0, 0.1) inset;
 
         .left {
             width: 40%;
